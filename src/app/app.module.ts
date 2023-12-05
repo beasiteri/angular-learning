315983e7966acdccc246311bcf32b36523fed6ef
beasiteri <siteribea@gmail.com>
2023-12-05 10:59:00 +0100

    Create posts component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
 import { MyReactiveFormComponent } from './my-reactive-form/my-reactive-form.component';
 import { MyFormArrayComponent } from './my-reactive-form/my-form-array/my-form-array.component';
+import { PostsComponent } from './http-services/posts/posts.component';
 
 @NgModule({
   declarations: [
@@ -13,11 +15,13 @@ import { MyFormArrayComponent } from './my-reactive-form/my-form-array/my-form-a
     TemplateDrivenFormComponent,
     MyReactiveFormComponent,
     MyFormArrayComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   exports: [
     TemplateDrivenFormComponent,
