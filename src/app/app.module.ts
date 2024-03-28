21d5d7bf999ffedc86482f7f3fcc0aef334d02f4
beasiteri <siteribea@gmail.com>
2024-03-28 14:58:49 +0100

    Define routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,12 @@ import { TemplateDrivenFormLearningComponent } from './modules/template-driven-f
 import { ReactiveFormLearningComponent } from './modules/reactive-form-learning/components/reactive-form-learning.component';
 import { FormArrayLearningComponent } from './modules/reactive-form-learning/components/form-array-learning/form-array-learning.component';
 import { SharedModule } from './shared/shared.module';
+import { NavbarComponent } from './modules/routing-learning/navbar/navbar.component';
+import { HomeComponent } from './modules/routing-learning/home/home.component';
+import { GithubProfileComponent } from './modules/routing-learning/github-profile/github-profile.component';
+import { NotFoundComponent } from './modules/routing-learning/not-found/not-found.component';
+import { RouterModule } from '@angular/router';
+import { GithubFollowersComponent } from './modules/routing-learning/github-followers/github-followers.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +23,11 @@ import { SharedModule } from './shared/shared.module';
     TemplateDrivenFormLearningComponent,
     ReactiveFormLearningComponent,
     FormArrayLearningComponent,
+    NavbarComponent,
+    HomeComponent,
+    GithubProfileComponent,
+    GithubFollowersComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
@@ -24,6 +35,15 @@ import { SharedModule } from './shared/shared.module';
     ReactiveFormsModule,
     HttpClientModule,
     SharedModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'http-learning', component: HttpLearningComponent },
+      { path: 'followers/:username', component: GithubProfileComponent },
+      { path: 'followers', component: GithubFollowersComponent },
+      { path: 'template-driven-form-learning', component: TemplateDrivenFormLearningComponent },
+      { path: 'reactive-form-learning', component: ReactiveFormLearningComponent },
+      { path: '**', component: NotFoundComponent },
+    ]),
   ],
   exports: [
     HttpLearningComponent,
